a28a7001f32a849340d985198638bc7447c640b0
maiam25 <maremaia70@pvnccdsb.on.ca>
2025-11-11 13:59:37 -0500

    nov.11

---
+++ Week11/Day11.2/student.js
@@ -0,0 +1,15 @@
+const student1 = {
+  name: "John Doe",
+  age: 20,
+  gender: "Male"
+};
+const student2 = {
+  name: "Jane Smith",
+  age: 22,
+  gender: "Female"
+};
+const student3 = {
+  name: "Alice Johnson",
+  age: 21,
+  gender: "Female"
+};
